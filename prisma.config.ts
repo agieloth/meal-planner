ee8b20a30c92205a40b2087d1a316ae063f5e240
aotchoun <aotchoun23@gmail.com>
2026-06-14 12:51:48 +0100

    fix: clean database and restore correct schema with 29 recipes

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,32 @@
+// import { defineConfig } from "prisma/config";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { Pool } from "pg";
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// export default defineConfig({
+//   // earlyAccess: true,
+//   schema: "prisma/schema.prisma",
+//   migrate: {
+//     adapter: new PrismaPg(pool),
+//   },
+// });
+
+
 import { defineConfig } from "prisma/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const connectionString = process.env.DATABASE_URL as string;
 
 export default defineConfig({
-  // earlyAccess: true,
   schema: "prisma/schema.prisma",
   migrate: {
-    adapter: new PrismaPg(pool),
+    adapter: () => {
+      const pool = new Pool({ connectionString });
+      return new PrismaPg(pool);
+    },
   },
 });
